c088c6e9a913fde94df38a16c336bd9e60eaecbc
cakoch10 <cakoch10@gmail.com>
2019-12-14 11:52:03 -0500

    fixed number of rounds and vec renormalization

--- src/run_ai.ml
+++ src/run_ai.ml
@@ -27,6 +27,12 @@ let update_strat strat1 strat2 hash vec turn =
               else (hash, vec)::str in
   if turn then (f strat1),strat2 else strat1,(f strat2)
 
+(** [update_hash strat1 strat2 hash vec turn] updates strat1 or strat2 depending
+ *  on turn with (hash,vec) replacing (hash, v) in the list *)
+let update_hash strat1 strat2 hash vec turn =
+  let f (h, v) = if h = hash then (h,vec) else (h,v) in
+  if turn then (List.map f strat1),strat2 else strat1,(List.map f strat2) 
+
 (** [init n f] is a tail recursive implementaiton of List.init (available after
  *  4.06.0 which returns the list [f 0;...; f n-1] *)
 let init n f =
@@ -34,6 +40,33 @@ let init n f =
     if i >= n then acc
     else init' ((f i)::acc) (i+1) n f
   in List.rev (init' [] 0 n f) 
+
+(* [get_new_vec valids] computes a prob vector from the list of valid moves 
+ * [valids] such that the probability is uniform over valid moves *)
+let get_new_vec valids =
+  let n = List.length valids in
+  if n = 0 then 
+    init 106 (fun i -> if i = 105 then 1.0 else 0.0)
+  else
+    let valid_ints = ints_from_moves valids in
+    let prob = 1.0 /. (float_of_int n) in
+    let f i =
+      if List.mem i valid_ints then prob else 0.0
+    in
+    init 106 f
+
+(** [renormalize_vec vec idx] takes a vector and zeroes out idx while 
+ *  redistributing the weight of the probability val at idx across the other 
+ *  nonzero entries *)
+let renormalize_vec vec idx =
+  let supp_size = 
+    (List.fold_left (fun a b -> if b <> 0.0 then a+1 else a) 0 vec) - 1 in
+  (* assume that a prob vec cannot have some move with prob = 1 *)
+  let prob_val = List.nth vec idx in
+  let renormal_factor = prob_val /. (float_of_int supp_size) in
+  List.mapi (fun i p -> 
+    if (p <> 0.0 && i <> idx) then p+.renormal_factor else 0.0
+  ) vec
 
 let log_last_command st =
   let str_st = last_cmd_to_str st in
@@ -45,6 +78,8 @@ let log_last_command st =
   Printf.fprintf oc "%s\n" loc_st;
   Printf.fprintf oc "%s\n" mov_st;
   Printf.fprintf oc "%s\n" hand_st;
+  let hash_st = string_of_int (hash_state st) in
+  Printf.fprintf oc "%s\n" hash_st;
   close_out oc
 
 (** [play_ai st strat1 strat2 turn] is the main loop for running the stategy
@@ -52,7 +87,11 @@ let log_last_command st =
  * which strategy to run in the immediate move
  * Requires: [st] is either an initial state or a state after a command 
  * has been executed *)
-let rec play_ai (st:state) (strat1:strategy) (strat2:strategy) (turn:bool) =
+let rec play_ai (st:state)
+                (strat1:strategy)
+                (strat2:strategy)
+                (turn:bool) 
+                (rnd:int) =
   let _ = log_last_command st in
   let strat = if turn then strat1 else strat2 in
   let hash = hash_state st in
@@ -62,16 +101,7 @@ let rec play_ai (st:state) (strat1:strategy) (strat2:strategy) (turn:bool) =
     with e ->
       (* need to update strategy and vec *)
       let valids = compute_valid_moves st in
-      let n = List.length valids in
-      if n = 0 then 
-        init 106 (fun i -> if i = 105 then 1.0 else 0.0)
-      else
-        let valid_ints = ints_from_moves valids in
-        let prob = 1.0 /. (float_of_int n) in
-        let f i =
-          if List.mem i valid_ints then prob else 0.0
-        in
-        init 106 f
+      get_new_vec valids
     in
   (* need to randomly sample according to strat vector *)
   let i = Random.float 1.0 in
@@ -100,13 +130,24 @@ let rec play_ai (st:state) (strat1:strategy) (strat2:strategy) (turn:bool) =
   if (winner st' <> "") then
     if "p1" = winner st' then save_winner "../Data/strategies/0.json" strat1
     else save_winner "../Data/strategies/1.json" strat2
-  else 
+  else if rnd > 100 then
+    if (curr_player_score st) > (last_player_score st) then
+      if turn then save_winner "../Data/strategies/0.json" strat1
+      else save_winner "../Data/strategies/1.json" strat2
+    else
+      if turn then save_winner "../Data/strategies/1.json" strat2
+      else save_winner "../Data/strategies/0.json" strat1
+  else
     match last_command st' with
-    | Err _ -> if turn then 
+    | Err _ -> 
+      let new_vec = renormalize_vec vec idx in
+      let strat1,strat2 = update_hash strat1 strat2 hash new_vec turn in
+      play_ai st' strat1 strat2 (not turn) (rnd+1)
+    (* | Err _ -> if turn then 
                 save_winner "../Data/strategies/1.json" strat 
-              else save_winner "../Data/strategies/0.json" strat
-    | End _ -> play_ai st' strat1 strat2 (not turn)
-    | _ -> play_ai st' strat1 strat2 turn
+              else save_winner "../Data/strategies/0.json" strat *)
+    | End _ -> play_ai st' strat1 strat2 (not turn) (rnd+1)
+    | _ -> play_ai st' strat1 strat2 turn (rnd+1)
   
 
 
@@ -121,7 +162,7 @@ let play_game f =
   let st = init_state (load_json (List.hd st_lst)) in
   let strat1 = load_strategy (load_json (List.nth st_lst 1)) in
   let strat2 = load_strategy (load_json (List.nth st_lst 2)) in
-  play_ai st strat1 strat2 true
+  play_ai st strat1 strat2 true 0
     
 
 (* [main ()] starts the REPL for the card game engine *)
