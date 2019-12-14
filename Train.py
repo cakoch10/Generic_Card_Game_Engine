5126cc6f9584580f47eefab0debf6c8accbfe43d
Justin Kae <justinkae@Justins-MacBook-Pro-2.local>
2019-12-14 00:15:49 -0500

    Training ready

--- Train.py
+++ Train.py
@@ -53,7 +53,7 @@ def load_generation_json(load_path, gen=0):
 """ [save_children] saves the generation of agents [child_dict] as an npz
     file in [save_path]. """
 def save_children_json(save_path, child_list):
-    print(child_list)
+    # print(child_list)
     try: 
         os.mkdir(save_path)
     except FileExistsError: 
@@ -67,17 +67,22 @@ def save_children_json(save_path, child_list):
 """ [reproduction] is the master function for reproducing a generation 
     to create a new one. [data_path] is the Data folder"""
 def reproduction(data_path="Data", gen=0): 
-    # parent_list = load_generation_json(data_path+"/Gen"+str(gen)+'/', gen)
     parent_list = load_generation_json(parent_directory)
-    parent_idx = np.arange(len(parent_list))
+    parent_idx = np.arange(int(len(parent_list)))
     child_list = []
+    # print(len(parent_list))
+    # matchings = np.random.choice(parent_idx, size=(len(parent_list)//2, 2), replace=False)
+    # matchings = matchings.tolist() + np.random.choice(parent_idx, size=(len(parent_list), 2), replace=True).tolist()
+    # print(matchings)
+    # for p1_i, p2_i in matchings: 
     for (p1_i, p2_i) in itertools.product(parent_idx, parent_idx):
         p1 = parent_list[p1_i]
         p2 = parent_list[p2_i]
         children_e = Agent.meiosis(p1, p2, Agent.merge_element)
         children_c = Agent.meiosis(p1, p2, Agent.merge_choose)
-        child_list = children_e + children_c 
+        child_list = child_list + children_e + children_c 
     save_children_json(data_path+"/Gen"+str(gen+1)+"/", child_list)
+    print(gen, len(child_list))
     return len(child_list)
 
 """ Returns a move based on agent [agent_name] and the state [st]. 
@@ -91,7 +96,6 @@ def reproduction(data_path="Data", gen=0):
     needed to be handled.  
     """
 def run_state(agent, st, prev_st=None, prev_move=None):
-    # agent = AGENT_DICT[agent_name]
     if prev_st == st and prev_move is not None: # Last move was invalid (state didn't change): 
         agent[st][prev_move] = 0
         agent[st] = Agent.normalize(agent[st])
@@ -106,7 +110,7 @@ def run_state(agent, st, prev_st=None, prev_move=None):
 
     Returns True if a1 wins, False if a2 wins, and a tuple of their scores"""
 def play_game(a1, a2, game, agent_directory):
-    print(agent_directory)
+    # print(agent_directory)
     process = Popen("/bin/bash", stdin=PIPE, stdout=PIPE, stderr=PIPE)
     commands = "cd src && make play_ai" + "\n"
     a1_dir = agent_directory+str(a1)+".json"
@@ -116,16 +120,16 @@ def play_game(a1, a2, game, agent_directory):
     if err:
         print("Error in executing command: " + str(err))
     # wait for result to be written
-    result1 = os.path.join(strategy_directory, "1.json")
-    result2 = os.path.join(strategy_directory, "2.json")
+    result1 = os.path.join(strategy_directory, "0.json")
+    result2 = os.path.join(strategy_directory, "1.json")
     os.listdir(strategy_directory)
     while not (os.path.exists(result1) or os.path.exists(result2)):
         time.sleep(0.5)
     if os.path.isfile(result1):
-        print("Got result1!")
+        # print("Got result1!")
         return True, result1
     else:
-        print("Got result2!")
+        # print("Got result2!")
         return False, result2
 
 """ [eval_generation] takes a generation of recently born children, and
@@ -139,7 +143,6 @@ def eval_generation(game, agent_directory, N):
         winner_bool, result = play_game(c1, c2, game, "."+agent_directory)
         # Handle metric by which to evaluate goodness. 
         winner = c1 if winner_bool else c2
-        # loser = c2 if winner_bool else c1
         winner_list.append(winner)
         # TODO: Move the winner to Data/Parents/ & delete from /Data/Strategies/
         # TODO: Make sure to rename to new i.json. 
@@ -148,24 +151,44 @@ def eval_generation(game, agent_directory, N):
     # Take top 10 or whatever of the children and return their indexes
     return winner_list
 
+def clear_dir(parent_directory): 
+    for file in os.listdir(parent_directory):
+        if file != ".DS_Store": 
+            os.remove(os.path.join(parent_directory, file))
+
 """ trains the model """
 def train(data_dir, game, max_gen=10):
     # loop over these two commands
     gen = 0
     while gen < max_gen:
         gen_size = reproduction(data_dir, gen)
+        print("Size of Gen"+str(gen)+':', gen_size)
         child_gen_dir = data_dir+"/Gen"+str(gen+1)+"/"
+        # os.rmdir(parent_directory)
+        clear_dir(parent_directory)
         winner_list = eval_generation(game, child_gen_dir, gen_size)
         print("Winners of Gen"+str(gen)+":", winner_list)
         # os.rename(parent_directory, data_dir+"/Gen"+str(gen+1)+"/")
         gen+=1
 
-NUM_OF_CHOICES = 106
+def make_gen0(gen_size): 
+    hash_list = [306253, 487054, 169059, 125452, 287134, 343487, 156313, 193547, 69099, 287134, 193547, 169059, 49980]
+    gen0_list = []
+    for _ in range(gen_size):
+        agent = {}
+        for each_hash in hash_list:
+            agent[each_hash] = Agent.gen_ran(NUM_OF_CHOICES)
+        gen0_list.append(agent)
+    return gen0_list
+    
 
+NUM_OF_CHOICES = 106
 path = '../Data'
 data_path = './Data'
-gen0_list = [{"0":Agent.perturb(np.zeros(NUM_OF_CHOICES))}, {"0":Agent.normalize(np.ones(NUM_OF_CHOICES))}]
-save_children_json("./Data/Gen0/", gen0_list)
+# gen0_list = [{"0":Agent.perturb(np.zeros(NUM_OF_CHOICES))}, {"0":Agent.normalize(np.ones(NUM_OF_CHOICES))}]
+clear_dir(parent_directory)
+gen0_list = make_gen0(4)
+save_children_json("./Data/Parents/", gen0_list)
 train(data_path, "crazy8_ai.json", 5)
 # AGENT_DICT = train("Data")
 # move = run_state("1_0", "1", "0", 0)
